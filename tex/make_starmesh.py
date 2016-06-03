9697986986527a80d60267c847c0c44041ecbaa9
Łukasz Rogalski <rogalski.91@gmail.com>
2016-06-04 01:20:07 +0200

    new figures

---
+++ tex/make_starmesh.py
@@ -0,0 +1,46 @@
+﻿from __future__ import division
+import math
+N = 5;
+r = 3;
+
+PREFIX = r"""
+\documentclass{article}
+\usepackage{tikz}
+\usepackage{circuitikz}
+\ctikzset{bipoles/length=1cm}
+\pagenumbering{gobble}
+\usepackage[paperheight=%fcm,paperwidth=%fcm,margin=0.2cm]{geometry}
+\begin{document}
+\begin{figure}[!ht]
+  \begin{center}
+    \begin{circuitikz}
+""" % (r*2.3, r*2.5)
+
+POSTFIX = r"""
+    \end{circuitikz}
+  \end{center}
+\end{figure}
+\end{document}
+"""
+
+def arg(i, N):
+	return 2*math.pi*i/N + N%2 * math.pi/2*N
+
+coords = [(r*math.cos(arg(i, N)), r*math.sin(arg(i, N))) for i in xrange(N)]
+
+with open('star.tex', 'w') as f:
+	f.write(PREFIX)
+	for c in coords:
+		f.write(" " * 6)
+		f.write("\\draw {0}\nto[R,d-*] (0,0);\n".format(c))
+	f.write(POSTFIX)
+
+with open('mesh.tex', 'w') as f:
+	f.write(PREFIX)
+	for c in coords:
+		for c2 in coords:
+			if c == c2:
+				continue
+			f.write(" " * 6)
+			f.write("\\draw {0}\nto[R,d-d] {1};\n".format(c, c2))
+	f.write(POSTFIX)
--- tex/run_all.sh
+++ tex/run_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pdflatex -interaction nonstopmode *.tex
-pdftops -eps *.pdf
-pdftops -jpg *.pdf
+python make_starmesh.py
+shopt -s nullglob
+for f in *.tex
+do
+	filename=$(basename "$f" .tex)
+	pdflatex -interaction nonstopmode $filename.tex
+	pdftops -eps $filename.pdf $filename.eps
+done
 rm *.aux *.auxlock *.dvi *.pdf *.log
